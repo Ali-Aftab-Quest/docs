f05f07fc2c7669ccc568cf4e2652b3ed0d3d417f
Ali Aftab <ali.aftab@questlab.pk>
2025-05-05 12:17:42 +0500

    added config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://Ali-Aftab-Quest.github.io',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Ali-Aftab-Quest', // GitHub username
+  projectName: 'subqc-docs', // Repo name
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

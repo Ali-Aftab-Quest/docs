074adf7f408bc01753199188145e8fdf92effc7c
Ali Aftab <ali.aftab@questlab.pk>
2025-05-05 12:27:31 +0500

    Deploy website - based on 3b364071f9c58041ef9b8a51dd0270e57f22e63e

---
+++ assets/js/acecf23e.a538e0b2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_documentation=self.webpackChunkmy_documentation||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/docs/blog","blogTitle":"Blog","authorsListPath":"/docs/blog/authors"}')}}]);
--- docs/work-orders.md
+++
@@ -1,7 +0,0 @@
----
-id: work-orders
-title: Work Orders
-sidebar_label: Work Orders
----
-
-Here we will discuss how to handle work orders.
